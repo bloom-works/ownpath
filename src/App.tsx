cc66a99c6ec9be814c62d92c8c09c9f24b233b43
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2022-07-27 15:09:09 -0400

    Demo: PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-
-import Layout from "./components/Layout";
-import Search from "./pages/Search/Search";
-import Home from "./pages/Home";
-import ResultDetail from "./pages/ResultDetail";
-import NotFound from "./pages/NotFound";
-import GuidedSearch from "./pages/GuidedSearch";
 
 function App() {
   const { t } = useTranslation();
