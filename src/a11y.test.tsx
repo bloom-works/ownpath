81e7687f8ae82dcf4a8332ccf5e81d23d9306eae
ElvisJG <Elvis.Gonzalez@bracari.com>
2022-07-28 17:27:58 -0400

    just missing env var for e2e

--- src/a11y.test.tsx
+++ src/a11y.test.tsx
@@ -2,50 +2,38 @@ import React from "react";
 import { render } from "./testHelper";
 import { axe } from "jest-axe";
 import Home from "./pages/Home";
+// import ResultDetail from "./pages/ResultDetail";
+import GuidedSearch from "./pages/GuidedSearch";
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search/Search";
 
+window.scrollTo = jest.fn();
+
+afterAll(() => {
+  jest.clearAllMocks();
+});
 test("Home", async () => {
   const { container } = render(<Home />);
   expect(await axe(container)).toHaveNoViolations();
 });
 
-// expect.extend(toHaveNoViolations);
-// test("Should not have basic a11y issues, ResultDetail", async () => {
+// ! Causes Maximum updates depth exceeded (inf)
+// test("ResultDetail", async () => {
 //   const { container } = render(<ResultDetail />);
 //   expect(await axe(container)).toHaveNoViolations();
 // });
 
-// it("Should not have basic a11y issues, Whoops", async () => {
-//   const { container } = render(
-//     <BrowserRouter>
-//       <Whoops />
-//     </BrowserRouter>
-//   );
-//   expect(await axe(container)).toHaveNoViolations();
-// });
+test("GuidedSearch", async () => {
+  const { container } = render(<GuidedSearch />);
+  expect(await axe(container)).toHaveNoViolations();
+});
 
-// it("Should not have basic a11y issues, Home", async () => {
-//   const { container } = render(
-//     <BrowserRouter>
-//       <Home />
-//     </BrowserRouter>
-//   );
-//   expect(await axe(container)).toHaveNoViolations();
-// });
+test("NotFound", async () => {
+  const { container } = render(<NotFound />);
+  expect(await axe(container)).toHaveNoViolations();
+});
 
-// it("Should not have basic a11y issues, GuidedSearch", async () => {
-//   const { container } = render(
-//     <BrowserRouter>
-//       <GuidedSearch />
-//     </BrowserRouter>
-//   );
-//   expect(await axe(container)).toHaveNoViolations();
-// });
-
-// it("Should not have basic a11y issues, Search", async () => {
-//   const { container } = render(
-//     <BrowserRouter>
-//       <Search />
-//     </BrowserRouter>
-//   );
-//   expect(await axe(container)).toHaveNoViolations();
-// });
+test("Search", async () => {
+  const { container } = render(<Search />);
+  expect(await axe(container)).toHaveNoViolations();
+});
