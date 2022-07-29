1809b9f54ba0d304855584a6a8daf9a887c7c03f
julia <hogan.julia@gmail.com>
2022-07-29 15:29:29 -0400

    PR updates

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -34,7 +34,7 @@ function Layout() {
                   <OwnPathLogo height={38} />
                 )}
               </a>
-              by
+              {t("components.layout.for")}
               <a
                 className="margin-x-2"
                 href="https://bha.colorado.gov/"
