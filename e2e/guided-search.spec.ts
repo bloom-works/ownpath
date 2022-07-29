795b7e2ff2bc29b0057d8a595202e63b2e5f5a61
ElvisJG <Elvis.Gonzalez@bracari.com>
2022-07-29 10:54:12 -0400

    commit before switch

---
+++ e2e/guided-search.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test";
+
+const baseURL = process.env.URL || "http://localhost:3000";
+test.beforeEach(async ({ page }) => {
+  await page.goto(baseURL);
+});
