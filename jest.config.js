569d9a713e2c4f6dfbffa89e78d703ed5cbd877f
ElvisJG <Elvis.Gonzalez@bracari.com>
2022-08-02 13:15:27 -0400

    removed config file

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  // setupFiles: ["<rootDir>/src/setupTests.ts"],
-};
