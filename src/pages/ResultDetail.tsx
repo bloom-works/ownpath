646e014b78305eda705fee82f5b97d947717ab20
Cathy Deng <cathyd125@gmail.com>
2022-08-18 14:48:31 -0700

    bugfix: don't show empty lists of services (#41)

--- src/pages/ResultDetail.tsx
+++ src/pages/ResultDetail.tsx
@@ -17,7 +17,7 @@ import DirectionsLink from "../components/ResultDetail/DirectionsLink";
 import ShareButton from "../components/ShareButton";
 import { logPageView } from "../analytics";
 import BackButton from "../components/BackButton";
-import { getMapMarker } from "../utils";
+import { anyAreTrue, getMapMarker } from "../utils";
 
 function ResultDetail() {
   // Ensure user sees the top of the page
@@ -127,32 +127,34 @@ function ResultDetail() {
           <Grid col={12} tablet={{ col: 6 }}>
             <section>
               <h2 className="margin-top-1">{t("services")}</h2>
-              {data.substanceUse.supported && (
-                <>
-                  <h3 className="display-inline">
-                    {t("substanceUseServices")}:
-                  </h3>
-                  <ul>
-                    <BulletedList
-                      boolMap={data.substanceUse.services}
-                      className="line-height-body-4"
-                    />
-                  </ul>
-                </>
-              )}
-              {data.mentalHealth.supported && (
-                <>
-                  <h3 className="display-inline">
-                    {t("mentalHealthServices")}:
-                  </h3>
-                  <ul>
-                    <BulletedList
-                      boolMap={data.mentalHealth.services}
-                      className="line-height-body-4"
-                    />
-                  </ul>
-                </>
-              )}
+              {data.substanceUse.supported &&
+                anyAreTrue(data.substanceUse.services) && (
+                  <>
+                    <h3 className="display-inline">
+                      {t("substanceUseServices")}:
+                    </h3>
+                    <ul>
+                      <BulletedList
+                        boolMap={data.substanceUse.services}
+                        className="line-height-body-4"
+                      />
+                    </ul>
+                  </>
+                )}
+              {data.mentalHealth.supported &&
+                anyAreTrue(data.mentalHealth.services) && (
+                  <>
+                    <h3 className="display-inline">
+                      {t("mentalHealthServices")}:
+                    </h3>
+                    <ul>
+                      <BulletedList
+                        boolMap={data.mentalHealth.services}
+                        className="line-height-body-4"
+                      />
+                    </ul>
+                  </>
+                )}
             </section>
           </Grid>
         )}
