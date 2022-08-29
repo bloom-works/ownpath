8d53e5005c6be351e8328a8fd78ec91b0efa2f6a
cathydeng <cathyd125@gmail.com>
2022-08-29 13:09:48 -0700

    more alert polish

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,8 +9,8 @@ import { ReactComponent as Close } from "../images/close.svg";
 import { ReactComponent as MiPropiaSendaLogo } from "../images/logos/mi_propia_senda.svg";
 import styled from "styled-components";
 import { useState } from "react";
-import AppAlert from "./AppAlert";
 import { useTranslation } from "react-i18next";
+import HighlightBox from "./HighlightBox";
 
 const Wrapper = styled.div`
   min-height: 100%;
@@ -27,9 +27,17 @@ function Layout() {
         <div className="display-flex flex-justify-center flex-align-center border-bottom border-base-lighter padding-y-2 padding-x-1">
           <a href="/" title="Home" aria-label="Home">
             {i18n.language === "es" ? (
-              <MiPropiaSendaLogo height={38} width="auto" title={t("ownPathLogoAlt")} />
+              <MiPropiaSendaLogo
+                height={38}
+                width="auto"
+                title={t("ownPathLogoAlt")}
+              />
             ) : (
-              <OwnPathLogo height={38} width="auto" title={t("ownPathLogoAlt")} />
+              <OwnPathLogo
+                height={38}
+                width="auto"
+                title={t("ownPathLogoAlt")}
+              />
             )}
           </a>
           <div className="margin-x-1 tablet:margin-x-2">{t("by")}</div>
@@ -40,8 +48,11 @@ function Layout() {
       </Header>
       {showCrisisAlert && location.pathname !== "/guided-search" && (
         <div className="margin-x-2 margin-top-2">
-          <AppAlert Icon={Phone}>
+          <HighlightBox size="sm">
             <div className="display-flex flex-justify">
+              <div className="display-none tablet:display-block">
+                <Phone className="data-icon margin-right-2" />
+              </div>
               <div>
                 <>
                   {t("crisisAlert")}{" "}
@@ -52,7 +63,7 @@ function Layout() {
               </div>
 
               <Button
-                className="width-auto margin-x-1"
+                className="width-auto margin-left-1"
                 type="button"
                 unstyled
                 title="close"
@@ -61,7 +72,7 @@ function Layout() {
                 <Close className="data-icon" />
               </Button>
             </div>
-          </AppAlert>
+          </HighlightBox>
         </div>
       )}
 
