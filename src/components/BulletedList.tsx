762a1e92d3fc73d4aa52352a083016318ec91f92
cathydeng <cathyd125@gmail.com>
2022-09-01 12:26:35 -0700

    handle empty populations/accessibility on provider detail page

--- src/components/BulletedList.tsx
+++ src/components/BulletedList.tsx
@@ -1,17 +1,24 @@
 import { useTranslation } from "react-i18next";
+import { anyAreTrue } from "../utils";
 
 type BulletedListProps = {
   boolMap: { [key: string]: boolean };
   translationPrefix?: string;
   className?: string;
+  emptyMsg?: string;
 };
 
 function BulletedList({
   boolMap,
   translationPrefix = "",
   className,
+  emptyMsg,
 }: BulletedListProps) {
   const { t } = useTranslation();
+
+  if (emptyMsg && !anyAreTrue(boolMap)) {
+    return <p>{emptyMsg}</p>;
+  }
 
   return (
     <>
