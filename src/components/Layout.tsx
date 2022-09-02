33d1239624e839dc5c3afa1176c46078316745ad
jlhogan <hogan.julia@gmail.com>
2022-09-02 10:05:58 -0400

    Persist language if changed using dropdown menu with URL params (#67)
    * Persist language if changed using dropdown menu with URL params
    
    * local storage
    
    * Fix accidental change from rebase

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
-import { Button, Grid, Header, Link } from "@trussworks/react-uswds";
-import { Outlet, useLocation } from "react-router-dom";
+import { Button, Header, Link as ExternalLink } from "@trussworks/react-uswds";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import Banner from "./Banner";
 import Footer from "./Footer";
 import { ReactComponent as ColoradoBhaLogo } from "../images/logos/colorado_bha.svg";
@@ -18,14 +18,15 @@ const Wrapper = styled.div`
 
 function Layout() {
   const [showCrisisAlert, setShowCrisisAlert] = useState(true);
-  const location = useLocation();
+
   const { t, i18n } = useTranslation();
+  const location = useLocation();
   return (
     <Wrapper className="display-flex flex-column">
       <Header basic color="primary" role="banner">
         <Banner />
         <div className="display-flex flex-justify-center flex-align-center border-bottom border-base-lighter padding-y-2 padding-x-1">
-          <a href="/" title="Home" aria-label="Home">
+          <Link to="/" title="Home" aria-label="Home">
             {i18n.language === "es" ? (
               <MiPropiaSendaLogo
                 height={34}
@@ -39,14 +40,18 @@ function Layout() {
                 title={t("ownPathLogoAlt")}
               />
             )}
-          </a>
+          </Link>
           <div className="margin-x-1 tablet:margin-x-2">{t("by")}</div>
-          <a href="https://bha.colorado.gov/" target="_blank" rel="noreferrer">
+          <ExternalLink
+            href="https://bha.colorado.gov/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <ColoradoBhaLogo
               height={i18n.language === "es" ? 32 : 34}
               width="auto"
             />
-          </a>
+          </ExternalLink>
         </div>
       </Header>
       {showCrisisAlert && location.pathname !== "/guided-search" && (
@@ -59,9 +64,12 @@ function Layout() {
                 </div>
                 <div>
                   {t("crisisAlert")}{" "}
-                  <Link href="tel:+18444938255" className="text-no-wrap">
+                  <ExternalLink
+                    href="tel:+18444938255"
+                    className="text-no-wrap"
+                  >
                     {t("crisisAlertNumber")}
-                  </Link>
+                  </ExternalLink>
                 </div>
               </div>
               <Button
