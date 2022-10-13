1b07b0e2ff8efeb42916e02ef15664324c9f4d8b
julia <hogan.julia@gmail.com>
2022-10-12 20:36:34 -0400

    Mobile filters in modal overlay

--- src/components/Search/Filters/Control/MobileControl.tsx
+++ src/components/Search/Filters/Control/MobileControl.tsx
@@ -1,5 +1,11 @@
-import { Button, Grid } from "@trussworks/react-uswds";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import {
+  Button,
+  Grid,
+  Modal,
+  ModalRef,
+  ModalToggleButton,
+} from "@trussworks/react-uswds";
+import { Dispatch, useRef, SetStateAction } from "react";
 import { useTranslation } from "react-i18next";
 import { AnalyticsAction, logEvent } from "../../../../analytics";
 import { SearchFilters, TypeOfHelp } from "../../../../types";
@@ -18,7 +24,7 @@ import AgeGroupInput from "../AgeGroupInput";
 import styled from "styled-components";
 import { ReactComponent as Close } from "../../../../images/close.svg";
 
-const FiltersButton = styled(Button)`
+const FiltersModalToggleButton = styled(ModalToggleButton)`
   font-size: 1.25rem;
   padding: 1rem 1.25rem;
 `;
@@ -27,48 +33,44 @@ type MobileControlProps = {
   filters: SearchFilters;
   setFilters: Dispatch<SetStateAction<SearchFilters>>;
   totalResultsCount: number;
-  isExpanded: boolean;
-  setIsExpanded: Dispatch<SetStateAction<boolean>>;
 };
 
 function MobileControl({
   filters,
   setFilters,
   totalResultsCount,
-  isExpanded,
-  setIsExpanded,
 }: MobileControlProps) {
+  const modalRef = useRef<ModalRef>(null);
   const { t } = useTranslation();
 
   const countSelected = getAppliedOptionalFiltersCount(filters);
-
   return (
     <div
       className="tablet:display-none"
       aria-hidden
       id="mobile-filter-container"
     >
-      <Grid
-        id="filter-close-bar"
-        row
-        className="position-sticky top-0 flex-justify-end bg-white z-top"
-      >
-        {isExpanded && (
-          <Button
+      <Modal id="mobile-filter-menu" ref={modalRef}>
+        <Grid
+          row
+          className="position-sticky top-neg-1 flex-justify flex-align-center bg-white z-top"
+        >
+          <h2 className="text-bold padding-y-2 display-flex flex-align-center margin-y-0">
+            <Filter className="margin-right-1 text-blue" height={30} />
+            {t("filteredBy")}
+            {"..."}
+          </h2>
+          <ModalToggleButton
+            modalRef={modalRef}
             className="width-auto margin-y-1"
             type="button"
             unstyled
-            title="close"
-            onClick={() => {
-              setIsExpanded(false);
-            }}
+            title="cancel"
+            closer
           >
-            {t("close")} <Close className="data-icon" />
-          </Button>
-        )}
-      </Grid>
-
-      <div className={isExpanded ? "display-block" : "display-none"}>
+            {t("cancel")} <Close className="data-icon margin-left-1" />
+          </ModalToggleButton>
+        </Grid>
         <div className="margin-y-3">
           <DistanceInput
             legend={t("distanceTitle")}
@@ -119,53 +121,45 @@ function MobileControl({
             setFilters={setFilters}
           />
         </div>
-      </div>
-      <div
-        className={`position-sticky bottom-0 padding-y-2 bg-white text-center ${
-          isExpanded ? "border-top border-base-lighter" : ""
-        }`}
-      >
-        <FiltersButton
-          type="button"
+        <div className="position-sticky bottom-neg-1 padding-y-2 bg-white text-center  border-top border-base-lighter">
+          <FiltersModalToggleButton
+            modalRef={modalRef}
+            type="button"
+            className="radius-pill display-flex flex-align-center flex-justify-center text-body-md"
+            outline={countSelected === 0}
+            base={countSelected !== 0}
+          >
+            {t("viewResults", { count: totalResultsCount })}
+          </FiltersModalToggleButton>
+          {countSelected > 0 && (
+            <Button
+              type="button"
+              className="margin-top-2 display-flex flex-align-center flex-justify-center"
+              onClick={() => {
+                logEvent(AnalyticsAction.ApplyFilter, {
+                  label: "Clear filters button",
+                });
+                setFilters(getFiltersWithOptionalCleared(filters));
+              }}
+              unstyled
+            >
+              {t(`clearAll`)}
+            </Button>
+          )}
+        </div>
+      </Modal>
+      <div className="position-sticky bottom-0 padding-y-2 bg-white text-center">
+        <FiltersModalToggleButton
+          modalRef={modalRef}
           className="radius-pill display-flex flex-align-center flex-justify-center text-body-md"
-          onClick={() => {
-            if (isExpanded) {
-              setIsExpanded(false);
-              window.scrollTo(0, 0);
-            } else {
-              setIsExpanded(true);
-              document.getElementById("filter-close-bar")?.scrollIntoView();
-            }
-          }}
           outline={countSelected === 0}
           base={countSelected !== 0}
         >
-          {isExpanded ? (
-            <>{t("viewResults", { count: totalResultsCount })} </>
-          ) : (
-            <>
-              <Filter className="margin-right-05" />
-              {t("toggleFiltersButton", {
-                count: countSelected,
-              })}
-            </>
-          )}
-        </FiltersButton>
-        {countSelected > 0 && (
-          <Button
-            type="button"
-            className="margin-top-2 display-flex flex-align-center flex-justify-center"
-            onClick={() => {
-              logEvent(AnalyticsAction.ApplyFilter, {
-                label: "Clear filters button",
-              });
-              setFilters(getFiltersWithOptionalCleared(filters));
-            }}
-            unstyled
-          >
-            {t(`clearAll`)}
-          </Button>
-        )}
+          <Filter className="margin-right-05" />
+          {t("toggleFiltersButton", {
+            count: countSelected,
+          })}
+        </FiltersModalToggleButton>
       </div>
     </div>
   );
