c73f9d1ad4bb471bebb1ada960c374a5e44a6805
julia <hogan.julia@gmail.com>
2022-10-31 11:49:58 -0400

    FIxup tests

--- src/a11y.test.tsx
+++ src/a11y.test.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { render } from "./testHelper";
 import { axe } from "jest-axe";
+import { MemoryRouter } from "react-router-dom";
+import MOCK_CARE_PROVIDER_DATA from "./data/ladders_data.json";
+import * as utils from "./utils";
+import FAQ from "./pages/FAQ";
 import Home from "./pages/Home";
 import ResultDetail from "./pages/ResultDetail";
 import GuidedSearch from "./pages/GuidedSearch";
 import NotFound from "./pages/NotFound";
 import Search from "./pages/Search/Search";
-import { MemoryRouter } from "react-router-dom";
-import CARE_PROVIDER_DATA from "./data/ladders_data.json";
-import FAQ from "./pages/FAQ";
 
 afterAll(() => {
   jest.clearAllMocks();
@@ -17,37 +18,56 @@ afterAll(() => {
 test("Home", async () => {
   const { container } = render(<Home />);
   expect(await axe(container)).toHaveNoViolations();
+  expect(document.activeElement).toBe(container.querySelector("h1"));
 });
 
 test("ResultDetail", async () => {
   const { container } = render(<ResultDetail />, {
     wrapper: ({ children }) => (
       <MemoryRouter
-        initialEntries={[{ state: { data: { ...CARE_PROVIDER_DATA[0] } } }]}
+        initialEntries={[
+          { state: { data: { ...MOCK_CARE_PROVIDER_DATA[0] } } },
+        ]}
       >
         {children}
       </MemoryRouter>
     ),
   });
   expect(await axe(container)).toHaveNoViolations();
+  expect(document.activeElement).toBe(container.querySelector("h1"));
 });
 
 test("GuidedSearch", async () => {
   const { container } = render(<GuidedSearch />);
   expect(await axe(container)).toHaveNoViolations();
+  expect(document.activeElement).toBe(container.querySelector("h1"));
 });
 
 test("NotFound", async () => {
   const { container } = render(<NotFound />);
   expect(await axe(container)).toHaveNoViolations();
+  expect(document.activeElement).toBe(container.querySelector("h1"));
 });
 
 test("Search", async () => {
-  const { container } = render(<Search />);
+  // Unfortunately I was unable to mock the JSON data set to be smaller subset,
+  // so just bumping timeout to allow application of filters to complete
+  // and component to render for testing
+  jest.setTimeout(1000 * 60 * 3);
+
+  const { container } = render(<Search />, {
+    wrapper: ({ children }) => (
+      <MemoryRouter initialEntries={[{ search: "?zip=80012" }]}>
+        {children}
+      </MemoryRouter>
+    ),
+  });
   expect(await axe(container)).toHaveNoViolations();
+  expect(document.activeElement).toBe(container.querySelector("h1"));
 });
 
 test("FAQ", async () => {
   const { container } = render(<FAQ />);
   expect(await axe(container)).toHaveNoViolations();
+  expect(document.activeElement).toBe(container.querySelector("h1"));
 });
