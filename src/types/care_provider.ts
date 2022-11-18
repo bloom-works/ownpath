7043e86c87de12a75427cacac2a2aae925e2d02d
Jesse Thach <jessethach@gmail.com>
2022-11-18 10:33:19 -0800

    Resolve code comments. Revert translation for feesValuesMedicaid. Add comment to substance.substanceUse.services type.

--- src/types/care_provider.ts
+++ src/types/care_provider.ts
@@ -135,6 +135,13 @@ export type CareProvider = {
   substanceUse: {
     supported: boolean;
     duiSupported: boolean;
+    /*
+    PeerSupport is derived from standalone column RSSOServicesProvided. The other services are
+    processed from a list of values contained in the SubstanceUseServices column. The values
+    in SubstanceUseServices are parsed and dynamically set in the process_data file. This type
+    relects the parsed values as keys from SubstanceUseServices and the additional PeerSupport property
+    that is set by the RSSOServicesProvided column.
+    */
     services: { [key in SubstanceUseServices]: boolean } & { PeerSupport: boolean };
   };
   mentalHealth: {
