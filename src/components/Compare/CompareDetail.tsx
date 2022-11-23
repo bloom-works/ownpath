a8c0f714a0ae6d4188f2c7bfa1d64969e436f13a
cdhs-bha-data-services-sys <108845323+cdhs-bha-data-services-sys@users.noreply.github.com>
2022-11-23 12:29:11 -0700

    Incorporate LADDERS push 20221118-145437557 (#176)
    * Automated LADDERS commit 20221118-145437557
    
    * Fixup styling for when both badges are present
    
    * Add to compare; hide in mobile
    
    * Reduce padding; add telelhealth tooltip
    
    Co-authored-by: julia <hogan.julia@gmail.com>

--- src/components/Compare/CompareDetail.tsx
+++ src/components/Compare/CompareDetail.tsx
@@ -6,6 +6,9 @@ import { useTranslation } from "react-i18next";
 import { CareProviderSearchResult } from "../../types";
 import DirectionsLink from "../ResultDetail/DirectionsLink";
 import { getMilesFromMeters } from "../../utils";
+import Badge from "../Badge";
+import { ReactComponent as BadgeCheck } from "../../images/badge-check.svg";
+import { ReactComponent as Telehealth } from "../../images/telehealth.svg";
 
 // TODO: add badges for telehealth and accepting new patients when we have them
 
@@ -24,6 +27,24 @@ function CompareDetail({
       <Link className="usa-link" to={`/result/${data.id}`}>
         <h2 className="margin-top-0 margin-bottom-3">{data.name}</h2>
       </Link>
+      <div className="margin-bottom-3 display-none tablet:display-flex flex-wrap">
+        {data.acceptingNewPatients && (
+          <Badge
+            bgColor="blue"
+            Icon={BadgeCheck}
+            text={t("acceptingNewPatients")}
+            showTooltip
+            tooltipText={t("acceptingNewPatientsNote")}
+          />
+        )}
+        {data.offersTelehealth && (
+          <Badge
+            bgColor="yellow"
+            Icon={Telehealth}
+            text={t("telehealthAvailable")}
+          />
+        )}
+      </div>
       <div className="margin-bottom-3 flex-fill">
         {data.phone && (
           <>
