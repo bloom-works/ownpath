8411ae3835a8b99c991d7cd71305c11f718f86a3
julia <hogan.julia@gmail.com>
2022-12-12 11:35:50 -0500

    Hack to get translated content and titles into Pagination; fix edge cases

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,6 @@
 import { Pagination } from "@trussworks/react-uswds";
 import { useEffect, useContext } from "react";
+import { useTranslation } from "react-i18next";
 import { PaginationContext } from "../pages/Search/Search";
 import { CareProviderSearchResult } from "../types";
 import { AnalyticsAction, logEvent } from "../utils/analytics";
@@ -9,6 +10,7 @@ function ResultsPagination({
 }: {
   results: CareProviderSearchResult[];
 }) {
+  const { t } = useTranslation();
   const { paginationConfig, setPaginationConfig } =
     useContext(PaginationContext);
   const pageNumber = paginationConfig.currentPage;
@@ -18,44 +20,81 @@ function ResultsPagination({
       ...paginationConfig,
       currentPage: 1,
       totalItems: results.length,
-      totalPages: Math.ceil(
-        paginationConfig.totalItems / paginationConfig.pageSize
-      ),
+      totalPages: Math.ceil(results.length / paginationConfig.pageSize),
     }));
-  }, []);
+  }, [results]);
 
-  // const clickNext = () => {
-  //   logEvent(AnalyticsAction.ClickPaginationButton, { pageNumber });
-  //   setPaginationConfig((paginationConfig) => ({
-  //     ...paginationConfig,
-  //     currentPage: paginationConfig.currentPage++,
-  //   }));
-  // };
+  // HACKKKKKK this component to inject translated content and add missing labels for svg icons
+  useEffect(() => {
+    // Set next button content to translated language, and add title to svg
+    if (document.querySelector("button.usa-pagination__next-page")?.innerHTML) {
+      (
+        document.querySelector(
+          "button.usa-pagination__next-page>span"
+        ) as HTMLElement
+      ).innerHTML = t("next");
 
-  // const clickPrevious = () => {
-  //   logEvent(AnalyticsAction.ClickPaginationButton, { pageNumber });
-  //   setPaginationConfig((paginationConfig) => ({
-  //     ...paginationConfig,
-  //     currentPage: paginationConfig.currentPage--,
-  //   }));
-  // };
+      (
+        document.querySelector(
+          "button.usa-pagination__next-page>svg"
+        ) as HTMLElement
+      ).setAttribute("title", t("next"));
+    }
+
+    // Set previous button content to translated language and add title to svg
+    if (
+      document.querySelector("button.usa-pagination__previous-page")?.innerHTML
+    ) {
+      (
+        document.querySelector(
+          "button.usa-pagination__previous-page > span"
+        ) as HTMLElement
+      ).innerHTML = t("previous");
+      (
+        document.querySelector(
+          "button.usa-pagination__previous-page>svg"
+        ) as HTMLElement
+      ).setAttribute("title", t("previous"));
+    }
+  });
+
+  const clickNext = () => {
+    logEvent(AnalyticsAction.ClickPaginationButton, { pageNumber });
+
+    setPaginationConfig(() => ({
+      ...paginationConfig,
+      currentPage: paginationConfig.currentPage + 1,
+    }));
+  };
+
+  const clickPrevious = () => {
+    logEvent(AnalyticsAction.ClickPaginationButton, { pageNumber });
+    setPaginationConfig(() => ({
+      ...paginationConfig,
+      currentPage: paginationConfig.currentPage - 1,
+    }));
+  };
 
   const clickPageNumber = (page: any) => {
     logEvent(AnalyticsAction.ClickPaginationButton, { pageNumber });
-    setPaginationConfig((paginationConfig) => ({
+    setPaginationConfig(() => ({
       ...paginationConfig,
       currentPage: parseInt(page.target.innerHTML),
     }));
   };
-
-  return (
+  return paginationConfig.totalPages > 1 ? (
     <Pagination
+      key={paginationConfig.currentPage}
       pathname=""
       currentPage={paginationConfig.currentPage}
+      onClickNext={clickNext}
+      onClickPrevious={clickPrevious}
       maxSlots={5}
       onClickPageNumber={clickPageNumber}
       totalPages={paginationConfig.totalPages}
     />
+  ) : (
+    <></>
   );
 }
 
