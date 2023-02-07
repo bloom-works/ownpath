0292bd38d0320808ed67f2b6f5d2c2633cd3428d
makaitakori <makai@bloomworks.digital>
2023-02-07 18:14:22 -0500

    implement provider search autocomplete functionality

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,6 +7,7 @@ import { ReactComponent as OwnPathLogo } from "../images/logos/ownpath.svg";
 import { ReactComponent as MiPropiaSendaLogo } from "../images/logos/mi_propia_senda.svg";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
+import ProviderSearchInput from "./ProviderSearch/ProviderSearchInput";
 
 const Wrapper = styled.div`
   min-height: 100%;
@@ -19,37 +20,41 @@ function Layout() {
     <Wrapper className="display-flex flex-column">
       <Header basic color="primary" role="banner">
         <Banner />
-        <div className="display-flex flex-justify-center flex-align-center border-bottom border-base-lighter padding-y-2 padding-x-1">
-          <Link to="/" title="Home" aria-label="Home">
-            <span className="usa-sr-only">{t("goToHomepage")}</span>
-            {i18n.language === "es" ? (
-              <MiPropiaSendaLogo
-                height={34}
+        <div className="display-flex flex-justify-center ">
+          <div className=" display-flex flex-justify-left border-bottom border-base-lighter padding-y-2 padding-x-1">
+            <Link to="/" title="Home" aria-label="Home">
+              <span className="usa-sr-only">{t("goToHomepage")}</span>
+              {i18n.language === "es" ? (
+                <MiPropiaSendaLogo
+                  height={34}
+                  width="auto"
+                  title={t("ownPathLogoAlt")}
+                />
+              ) : (
+                <OwnPathLogo
+                  height={38}
+                  width="auto"
+                  title={t("ownPathLogoAlt")}
+                />
+              )}
+            </Link>
+            <div className="margin-x-1 tablet:margin-x-2">{t("by")}</div>
+            <ExternalLink
+              href="https://bha.colorado.gov/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ColoradoBhaLogo
+                height={i18n.language === "es" ? 32 : 34}
                 width="auto"
-                title={t("ownPathLogoAlt")}
               />
-            ) : (
-              <OwnPathLogo
-                height={38}
-                width="auto"
-                title={t("ownPathLogoAlt")}
-              />
-            )}
-          </Link>
-          <div className="margin-x-1 tablet:margin-x-2">{t("by")}</div>
-          <ExternalLink
-            href="https://bha.colorado.gov/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <ColoradoBhaLogo
-              height={i18n.language === "es" ? 32 : 34}
-              width="auto"
-            />
-          </ExternalLink>
+            </ExternalLink>
+          </div>
+          <div className="display-flex flex-justify-end">
+            <ProviderSearchInput />
+          </div>
         </div>
       </Header>
-
       <div className="flex-1">
         <Outlet />
       </div>
---
+++ src/components/ProviderSearch/ProviderSearchInput.tsx
@@ -0,0 +1,49 @@
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import CARE_PROVIDER_DATA from "../../data/ladders_data.json";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+type Item = {
+  id: string;
+  name: string;
+};
+
+// Grab id and name from CareProvider data
+const providersList = CARE_PROVIDER_DATA.map((provider) => ({
+  id: provider.id,
+  name: provider.name,
+}));
+
+function ProviderSearchInput({}) {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  // Take user to provider detail page when they click a provider name
+  const handleOnSelect = () => {
+    console.log("providersList", providersList);
+    // navigate({
+    //   pathname: `/result/${providerId}`,
+    // });
+  };
+
+  return (
+    <div>
+      <div
+        style={{ width: 300, marginLeft: 100, marginBottom: 20, marginTop: 20 }}
+      >
+        <ReactSearchAutocomplete<Item>
+          items={providersList}
+          placeholder={t("Search providers")}
+          fuseOptions={{ keys: ["name"] }}
+          resultStringKeyName="name"
+          maxResults={6}
+          onSelect={handleOnSelect}
+          styling={{ zIndex: 4 }} // To display it on top of the search box below
+          autoFocus={false}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ProviderSearchInput;
