85a4251169d29d42ed08d229880f41d7e9d30f38
jlhogan <hogan.julia@gmail.com>
2023-04-27 13:05:51 -0400

    Add Banner with downtime/maintenance notification (#427)
    * Add Banner with downtime/maintenance notification
    
    * Updates

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,6 +3,7 @@ import { Dropdown, Grid, Label } from "@trussworks/react-uswds";
 import { useTranslation } from "react-i18next";
 
 import { ReactComponent as Globe } from "../images/globe.svg";
+import { ReactComponent as Info} from "../images/info.svg";
 import caretDownURL from "../images/caret-down.svg";
 
 const StyledDropdown = styled(Dropdown)`
@@ -20,10 +21,20 @@ const StyledDropdown = styled(Dropdown)`
   background-size: 15%;
 `;
 
+
+const MaintenanceBanner = styled.div`
+  background-color: #F8D347;
+`
+
 function Banner() {
   const { t, i18n } = useTranslation();
 
   return (
+    <>
+    <MaintenanceBanner className="Banner bg-secondary-dark text-black font-body-xs padding-y-1 padding-x-3 desktop:padding-x-0 display-flex flex-align-center flex-justify-center">
+      <Info className="margin-right-1 display-none desktop:display-block" /> 
+      OwnPath is getting better! Scheduled maintenance will be from 11am to 2pm today. If the page doesn't load, please refresh.
+    </MaintenanceBanner>
     <div className="Banner usa-dark-background font-body-3xs margin-bottom-1">
       <Grid row className="flex-justify-end">
         <Grid col="auto" className="padding-x-2">
@@ -52,6 +63,7 @@ function Banner() {
         </Grid>
       </Grid>
     </div>
+    </>
   );
 }
 
